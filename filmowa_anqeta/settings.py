dc7bdeda807c86326f57e6e08ec54ee227686de2
Rafal Gebski <rafgeb@gmail.com>
2019-10-07 20:23:51 +0200

    heroku compatibility

--- filmowa_anqeta/settings.py
+++ filmowa_anqeta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'filmowa_anqeta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'de5c5roa45c8e0'),
     }
 }
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
